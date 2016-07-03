b705f9405f2e7a0385073f0839d8077a1e9f653c
Rahul Mahadev <rahul.mahadev7@gmail.com>
2016-07-03 18:02:23 +0530

    Added aes enc/decrypt

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@
 #include "tee.h"
 #include "client_controller.h"
 #include "helper.h"
-
+#include "aes.h"
 /* constants */
 #define APP_PRIORITY seL4_MaxPrio
 #define APP_IMAGE_NAME "hello-4-app"
@@ -13,56 +13,56 @@
 
 
 /*
- *                  boot   -----   process
- *                 /                    \
- *              RICH OS               RICH-syscall interface       
- *                                         |
- *                                         TA   
+ *				  boot   -----   process
+ *				 /					\
+ *			  RICH OS			   RICH-syscall interface	   
+ *										 |
+ *										 TA   
  *
  */
 
 int main(void)
 {
-    UNUSED int error;
-    simple_t simple;
-    vka_t vka;
-    allocman_t *allocman = NULL;
-    vspace_t vspace;
-    seL4_BootInfo *info = seL4_GetBootInfo();
+	UNUSED int error;
+	simple_t simple;
+	vka_t vka;
+	allocman_t *allocman = NULL;
+	vspace_t vspace;
+	seL4_BootInfo *info = seL4_GetBootInfo();
 
-    init_tee(&simple,info);
+	init_tee(&simple,info);
 
-    get_allocators(allocman,&vspace,&vka,&simple,info);
-    printf("tee-container: Starting rich OS\n");
-    //start client os
-    // 
-     // vspace_t client_vspace;
-    // error = get_new_vspace(&vspace,&client_vspace,&vka,&simple);
-    sel4utils_process_t client_proc;
-    cspacepath_t ep_cap_path;
-    start_rich_os(&client_proc,&vka,&vspace,&ep_cap_path);
-    listener(&ep_cap_path,&vka,&vspace,&client_proc);
-    // send_msg(&client_proc,&ep_cap_path);
-    //
-    //start ta
-    // vspace_t ta_vspace;
+	get_allocators(allocman,&vspace,&vka,&simple,info);
+	printf("tee-container: Starting rich OS\n");
+	//start client os
+	// 
+	 // vspace_t client_vspace;
+	// error = get_new_vspace(&vspace,&client_vspace,&vka,&simple);
+	sel4utils_process_t client_proc;
+	cspacepath_t ep_cap_path;
+	start_rich_os(&client_proc,&vka,&vspace,&ep_cap_path);
+	listener(&ep_cap_path,&vka,&vspace,&client_proc);
+	// send_msg(&client_proc,&ep_cap_path);
+	//
+	//start ta
+	// vspace_t ta_vspace;
 
-    // error = get_new_vspace(allocman,&vspace,&ta_vspace,&vka,&simple);
-   
-    while(1){
+	// error = get_new_vspace(allocman,&vspace,&ta_vspace,&vka,&simple);
 
-    }
+	while(1){
 
-    // vspace_t ta2_vspace;
-    // error = get_new_vspace(&ta2_vspace,&vka,&simple);
-    // trusted_app_t new_app2;
-    // init_ta(&new_app2,&vka,&vspace,APP_IMAGE_NAME);
-    // start_ta(&new_app,&vka,&ta_vspace,APP_IMAGE_NAME);
-    // char arr[51] = "This is a sample text 0\0";
-    // call_function(&new_app,6,HELLO_TA_INCREMENT,arr,sizeof(char) * 50);
-    //Only single function call works
-    //call_function(&new_app,5,HELLO_TA_INCREMENT);
+	}
+
+	// vspace_t ta2_vspace;
+	// error = get_new_vspace(&ta2_vspace,&vka,&simple);
+	// trusted_app_t new_app2;
+	// init_ta(&new_app2,&vka,&vspace,APP_IMAGE_NAME);
+	// start_ta(&new_app,&vka,&ta_vspace,APP_IMAGE_NAME);
+	// char arr[51] = "This is a sample text 0\0";
+	// call_function(&new_app,6,HELLO_TA_INCREMENT,arr,sizeof(char) * 50);
+	//Only single function call works
+	//call_function(&new_app,5,HELLO_TA_INCREMENT);
   
-    return 0;
+	return 0;
 }
 
